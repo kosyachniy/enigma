ac7ff6cca95dac53e00b203834675898fd4aa7b6
Poloz Alexey <polozhev@mail.ru>
2017-04-28 01:32:34 +0300

    Change structure

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 def alpha(text):
 	return text
 
-#Коммутационную панель
-def panel(patch,text):
-	for i in patch:
-		if i[0]==text:
-			return i[1]
-
-#Роторы
-def there(inp,shift,alphabet,patch):
-	print(inp)
-	return panel(patch,alphabet[(int(alphabet.index(inp)+shift)%len(alphabet))])
-
-def here(inp,shift,alphabet,patch):
-	return panel(patch,alphabet[(int(alphabet.index(inp)-shift)%len(alphabet))])
-
+#Ввод данных
 #key=(int(i) for i in input())
 key=[]
 nom=input()
@@ -26,15 +13,29 @@ n=len(text)
 alphabet=tuple(i for i in set(j for j in text+'йцукенгшщзхъёфывапролджэячсмитьбюqwertyuiopasdfghjklzxcvbnm'))
 text=list(text)
 
+#Коммутационная панель
+def panel(text):
+	for i in patch:
+		if i[0]==text:
+			return i[1]
+
+#Роторы
+def there(inp,shift):
+	return panel(alphabet[(int(alphabet.index(inp)+shift)%len(alphabet))])
+
+def here(inp,shift):
+	return panel(alphabet[(int(alphabet.index(inp)-shift)%len(alphabet))])
+
 for i in key:
 	for j in range(n):
-		text[j]=there(text[j],i,alphabet,patch)
+		text[j]=there(text[j],i)
+#Рефлектор
 for j in range(n//2):
 	text[j],text[n-1-j]=text[n-1-j],text[j]
 #Изменить структуру валов / роторов и поставить here
 for i in key[::-1]:
 	for j in range(n):
-		text[j]=there(text[j],i,alphabet,patch)
+		text[j]=there(text[j],i)
 
 for i in text:
 	print(i,end='')
