33dc0551f188a95f0cb9fadf087b962dfa1c83a0
Poloz Alexey <polozhev@mail.ru>
2017-04-28 14:00:56 +0300

    New method of my function

--- my.py
+++ my.py
@@ -15,28 +15,46 @@ def my(text,alphabet,n,m):
 			i-=1
 		return x
 
-	def de(text,di):
+	def de(text,di,ma):
+		x=[]
+		'''
 		j=text[0]
 		num=0
-		x=[]
 		text.append((text[len(text)-1]+1)%di)
 		for i in text:
 			if i==j:
-				if num>=m:
-					x.append(num-1)
+				if num>=ma:
+					x.append(num-1) !!!
 					num=1
+					!!!
 				else:
 					num+=1
 			else:
-				x.append(num-1)
+				x.append(num-1) !!!
 				num=1
 				j=(j+1)%di
+		'''
+
+		j=0
+		while j<=len(text)-1:
+			for i in range(di):
+				k=0
+				while (j<=len(text)-1) and (text[j]==i):
+					if k>=ma:
+						x.append(k)
+						x.append([0]*(di-1))
+						k=1
+					else:
+						k+=1
+					j+=1
+				x.append(k)
+
 		return x
 
 	st=[0]
 	for i in text:
 		st+=bin(i)
-	st=de(st,2)
+	st=de(de(st,2,m),m-1,m)
 	new=''
 	for i in st:
 		new+=alphabet[i]
